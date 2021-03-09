d5c4d1b49f3e738f451ef726459b9ef8e2e05400
Manikants98 <68854278+Manikants98@users.noreply.github.com>
2021-03-10 01:55:45 +0530

    Delete priority-based-cab-search-engine-master directory

--- priority-based-cab-search-engine-master/app/src/main/java/com/fw/olauberintegration/api/request/base/BasicResponseData.java
+++
@@ -1,6 +0,0 @@
-package com.fw.olauberintegration.api.request.base;
-
-
-
-public class BasicResponseData extends ResponseData {
-}
--- priority-based-cab-search-engine-master/app/src/main/java/com/fw/olauberintegration/api/request/base/RequestData.java
+++
@@ -1,6 +0,0 @@
-package com.fw.olauberintegration.api.request.base;
-
-
-
-public abstract class RequestData {
-}
